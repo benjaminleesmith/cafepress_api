7998a10577c705f85a3f330ab1b46a7fce182dcc
Benjamin Smith <benjamin.lee.smith@gmail.com>
2011-03-01 00:15:54 -0700

    added a mapping between merchandise and genders

--- lib/cafepress_api.rb
+++ lib/cafepress_api.rb
@@ -16,14 +16,17 @@
 
 require 'rexml/document'
 require 'open-uri'
+require 'product_genders'
 module CafePressAPI
   RESULTS_PER_PAGE = 100
+  include ProductGenders
 
   def self.store_url(cafepress_store_id)
     "http://cafepress.com/#{cafepress_store_id}"
   end
 
-  #Generates a url to add one product to a CafePress cart.
+  # Generates a url to add one product to a CafePress cart.
+  # This isn't part of the CafePress API, use at your own risk!
   def self.add_to_cart_url(product_id, size, color, quantity, keep_shopping_url)
     "http://www.cafepress.com/cp/addtocart.aspx?color_#{product_id}=#{color}&size_#{product_id}=#{size}&qty_#{product_id}=#{quantity}&keepshopping=#{keep_shopping_url}&storeid=search"
   end
@@ -46,13 +49,28 @@ module CafePressAPI
         cafepress_back_design_id = nil
       end
 
+      # The CafePress API doesn't have a concept of genders... so I'm faking it
+      # Gender mapping was created by hand, use at own risk
+      if (gender = PRODUCT_GENDERS[product.attributes['merchandiseId']]).nil?
+        gender = PRODUCT_GENDERS[product.attributes['defaultCaption']]
+        if gender.nil?
+          gender = UNISEX
+          warn(%{
+\nWARNING: the product with defaultCaption '#{product.attributes['defaultCaption']}' and merchandiseId #{product.attributes['merchandiseId']} does not exist in the gender mapping!\n
+Please send benjamin.lee.smith@gmail.com an email and copy/paste this warning message.\n
+Or better yet, add the mapping yourself in product_genders.rb and submit it back https://github.com/benjaminleesmith/cafepress_api\n
+%})
+        end
+      end
+
       products << {
         :name => product.attributes['name'],
         :default_caption => product.attributes['defaultCaption'],
         :cafepress_product_id => product.attributes['id'],
         :url => product.attributes['marketplaceUri'],
         :cafepress_design_id => product.get_elements("mediaConfiguration[@perspectives='Front']").first.attributes['designId'],
-        :cafepress_back_design_id => cafepress_back_design_id
+        :cafepress_back_design_id => cafepress_back_design_id,
+        :gender => gender # See comment above
       }
     end
     products
