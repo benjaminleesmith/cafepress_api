9ccc7f231ed22219269661993dd3cbc2d3a49645
Benjamin Smith <benjamin.lee.smith@gmail.com>
2011-03-06 23:28:22 -0700

    added a product sizes module, but decided against implementing it for now

---
+++ lib/product_sizes.rb
@@ -0,0 +1,22 @@
+# Copyright 2011 Benjamin Lee Smith <benjamin.lee.smith@gmail.com>
+#
+# This file is part of CafePressAPI.
+# CafePressAPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CafePressAPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CafePressAPI.  If not, see <http://www.gnu.org/licenses/>.
+
+# I was going to create some static hash for sizes and their descriptions, but
+# since price is linked directly to size and price seems highly volatile, I'm
+# deciding a static hash for sizes would be pointless since you have to parse
+# the size elements anyway.
+module ProductSizes
+end
